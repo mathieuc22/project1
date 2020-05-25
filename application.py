3d2180a9a27cafe07284f99fa7edeceb5daef92d
Mathieu <mathieu.collet@gmail.com>
2020-05-26 01:16:01 +0200

    change registration login and logoutprocess and layout

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import os
 import requests
 
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, flash
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -27,23 +27,67 @@ def index():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        books = db.execute("SELECT * FROM books fetch first 10 rows only").fetchall()
-        return render_template("index.html", books=books)
+        return render_template("index.html")
 
-@app.route('/login', methods=['POST'])
-def login():
-
-    username = str(request.form.get("username"))
-    password = str(request.form.get("password"))
-
-    logged_in = db.execute("SELECT * FROM users WHERE LOWER(name) = LOWER(:username) AND  password = :password",
-            {"username": username, "password": password}).fetchone()
-
-    if logged_in:
-        session['logged_in'] = True
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if session.get('logged_in'):
+        flash("You're already logged in. Please log out.")
+        return render_template("index.html")
     else:
-        return render_template("error.html", message="Wrong password!")
-    return index()
+        if request.method == 'POST':
+
+            # Get form information.
+            username = str(request.form.get("username")) or None
+            email = str(request.form.get("email")) or None
+            password = str(request.form.get("password")) or None
+
+            user = db.execute("SELECT * FROM users WHERE LOWER(name) = LOWER(:username)",
+                {"username": username}).fetchone()
+
+            if user:
+                flash("You're already in our system. Please login.")
+                return render_template("login.html")
+            elif all(v is not None for v in [username, email, password]):
+                db.execute("INSERT INTO users (name, email, password) VALUES (:username, :email, :password)",
+                {"username": username, "email": email, "password": password})
+                db.commit()
+                logged_in = db.execute("SELECT * FROM users WHERE LOWER(name) = LOWER(:username)",
+                    {"username": username}).fetchone()
+                session['logged_in'] = True
+                session['user_id'] = logged_in[0]
+                session['user_name'] = logged_in[1]
+                #redirect to home
+                flash("Registration Successful. Your are logged in.")
+                return render_template("index.html")
+            else:
+                flash("Please correct data")
+                return render_template("register.html")
+        else:
+            return render_template("register.html")
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if session.get('logged_in'):
+        flash("You're already logged in. Please log out.")
+        return render_template("index.html")
+    else:
+        if request.method == 'POST':
+            username = str(request.form.get("username"))
+            password = str(request.form.get("password"))
+            logged_in = db.execute("SELECT * FROM users WHERE LOWER(name) = LOWER(:username) AND  password = :password",
+                    {"username": username, "password": password}).fetchone()
+
+            if logged_in:
+                session['logged_in'] = True
+                session['user_id'] = logged_in[0]
+                session['user_name'] = logged_in[1]
+            else:
+                 flash("Please try again or register.")
+                 return render_template("login.html")
+            return render_template("index.html")
+        else:
+            return render_template("login.html")
 
 @app.route("/logout")
 def logout():
@@ -96,7 +140,6 @@ def book(book_isbn):
         ratings_count = data["books"][0]["ratings_count"]
         average_rating = data["books"][0]["average_rating"]
 
-        users = db.execute("SELECT * FROM users").fetchall()
         reviews = db.execute("SELECT * FROM reviews").fetchall()
 
         # Make sure book exists.
@@ -104,7 +147,8 @@ def book(book_isbn):
         if book is None:
             return render_template("error.html", message="No such book.")
 
-        return render_template("book.html", book=book, book_isbn=book_isbn, ratings_count=ratings_count, average_rating=average_rating, users=users, reviews=reviews)
+        user_name = db.execute("SELECT name FROM users JOIN reviews ON reviews.user_id = users.id;").fetchone()
+        return render_template("book.html", book=book, ratings_count=ratings_count, average_rating=average_rating, reviews=reviews, user_name=user_name)
 
 @app.route("/review", methods=["POST"])
 def review():
@@ -114,14 +158,27 @@ def review():
         # Get form information.
         rating = request.form.get("rating")
         content = request.form.get("content")
-        user = request.form.get("user")
         isbn = request.form.get("isbn")
+        user_id = session.get('user_id')
 
-        # Make sure the flight exists.
+        # Make sure the book exists.
         if db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).rowcount == 0:
           return render_template("error.html", message="No such book with that isbn.")
         db.execute("INSERT INTO reviews (rating, content, date, isbn, user_id) VALUES (:rating, :content, current_timestamp, :isbn, :user_id)",
-              {"rating": rating, "content": content, "isbn": isbn, "user_id": user})
+              {"rating": rating, "content": content, "isbn": isbn, "user_id": user_id})
+        db.commit()
+        return render_template("success.html")
+
+@app.route("/delete_review", methods=["POST"])
+def delete_review():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        book_isbn = str(request.form.get("book_isbn"))
+        user_id = session.get('user_id')
+
+        db.execute("DELETE FROM reviews WHERE user_id = :user_id AND isbn = :isbn",
+            {"user_id": user_id, "isbn": book_isbn})
         db.commit()
         return render_template("success.html")
 
